51ac7d86b9c18eefe52fe044758ca1a709c4a5f1
diego <diegodamibenitez@gmail.com>
2020-01-15 20:52:33 -0300

    se creo registro de usuarios y mejoro el front

--- core/url.py
+++ core/url.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, listado_producto, nuevo_producto, modificar_producto, eliminar_producto
+from .views import home, listado_producto, nuevo_producto, modificar_producto, eliminar_producto, registro_usuario, bienvenida
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,6 +7,9 @@ urlpatterns = [
     path('nuevo-producto', nuevo_producto, name='nuevo_producto'),
     path('modificar-producto/<id>/', modificar_producto, name='modificar_producto'),
     path('eliminar-producto/<id>/', eliminar_producto, name='eliminar_producto'),
+    path('registro/', registro_usuario , name='registro_usuario'),
+    path('bienvenida/', bienvenida, name='bienvenida'),
+
 
 
 
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Producto
-from .forms import ProductoForm
+from .forms import ProductoForm, CustomUserForm
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login, authenticate
+
 
 def home(request):
     return render(request,'core/home.html')
@@ -54,3 +56,23 @@ def eliminar_producto(request, id):
 
     return redirect(to='listado_producto')
 
+def registro_usuario(request):
+    data = {
+        'form':CustomUserForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustomUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            #autentificar al usuario y redirigido al inicio
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            return redirect(to='bienvenida')
+
+    return render(request, 'registration/registrar.html', data)
+
+def bienvenida(request):
+    return render(request,'core/bienvenida.html')
